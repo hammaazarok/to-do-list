66e8e126123203a55972672f3e237215628bcddf
Hamma Azarok <hammaagazarok@gmail.com>
2022-07-05 15:56:53 +0200

    add tasks array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import './index.css';
+
+let tasks = [
+    {
+        description:"Write article",
+        completed:false,
+        index:0
+    },
+    {
+        description:"Go shop",
+        completed:false,
+        index:1
+    },
+    {
+        description:"Complete 2 coding challenges",
+        completed:false,
+        index:2
+    }
+]
+
 function component() {
-    const divElement = document.createElement('div');
-    const h2 = document.createElement('h2');
-    h2.innerText = 'My first webpack setup';
-    divElement.appendChild(h2);
-    return divElement;
+    // const divElement = document.createElement('div');
+    // const li = document.createElement('li');
+    // h2.innerText = 'My first webpack setup';
+    // divElement.appendChild(h2);
+    // return divElement;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
