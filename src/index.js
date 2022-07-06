8d819ee2ed78a76c5154f4b4fb693d90df04bdd4
Hamma Azarok <hammaagazarok@gmail.com>
2022-07-07 00:42:20 +0200

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import "./index.css";
-import * as todoList from "./todolist.js";
-import * as addRemove from "./add-remove.js";
-import * as storage from "./storage.js";
+import './index.css';
+import * as todoList from './todolist.js';
+import * as addRemove from './add-remove.js';
+import * as storage from './storage.js';
 
 const tasks = storage.getTasksFromStorage();
 
 todoList.TodoListItems(tasks);
 addRemove.TodoListDelete(tasks);
-const addTask = document.querySelector(".text-area");
-addTask.addEventListener("keydown", (e) => {
+const addTask = document.querySelector('.text-area');
+addTask.addEventListener('keydown', (e) => {
   if (e.keyCode === 13) {
     const task = {
       description: addTask.value,
@@ -20,20 +20,19 @@ addTask.addEventListener("keydown", (e) => {
   }
 });
 
-const editTask = document.querySelectorAll(".item");
-editTask.forEach((i,index)=>{
-  i.addEventListener("keydown", (e) => {
+const editTask = document.querySelectorAll('.item');
+editTask.forEach((i, index) => {
+  i.addEventListener('keydown', (e) => {
     if (e.keyCode === 13) {
       const task = {
         description: i.value,
         completed: false,
-        index: index,
+        index,
       };
       tasks[index] = task;
       todoList.TodoListItems(tasks);
       storage.setTasksToStorage(tasks);
-      addRemove.TodoListDelete(tasks)
+      addRemove.TodoListDelete(tasks);
     }
   });
-})
-
+});
